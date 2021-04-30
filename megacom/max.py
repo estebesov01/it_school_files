4b0061f8d2d87a434c61866d4f668dcbc231698f
nurs <estebesov7777@gmail.com>
2021-04-30 21:21:36 +0600

    qwe

--- megacom/max.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Введите целое: "))
-num1 = num
-sum = 0
-nums = []
-while (num != 0):
-    nums.append(num%10)
-    num = num // 10
-print(f"Наибольшая цифра в числе {num1} = {max(nums)}")
--- megacom/summofint.py
+++
@@ -1,6 +0,0 @@
-num = int(input("Введите целое: "))
-sum = 0
-while (num != 0):
-    sum = sum + num % 10
-    num = num // 10
-print("Сумма цифр числа равна: ", sum)
